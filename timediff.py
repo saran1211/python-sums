a3481745de12ff1d89865ea9fd863df14443ca75
saran1211 <36408495+saran1211@users.noreply.github.com>
2018-05-24 11:24:53 +0530

    timediff

---
+++ timediff.py
@@ -0,0 +1,8 @@
+a=int(input('enter the val of h1'))
+b=int(input('enter the val of m1'))
+c=int(input('enter the val of h2'))
+d=int(input('enter the val of m2'))
+if (b>60 and d>60):
+  a=a-1
+  c=c-1
+print(a-c,b-d)
